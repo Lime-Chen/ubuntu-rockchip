fbf70548e40059f4be83815d84ed60bcec95e770
Lime-Chen <lemon1151@163.com>
2024-11-19 00:21:44 +0800

    Update oracular.sh

--- config/suites/oracular.sh
+++ config/suites/oracular.sh
@@ -1,10 +1,10 @@
 # shellcheck shell=bash
 
-export RELASE_NAME="Ubuntu 24.10 (Oracular Oriole)"
-export RELASE_VERSION="24.10"
+export RELASE_NAME="Ubuntu 24.04 LTS (Noble Nombat)"
+export RELASE_VERSION="24.04"
 
 export KERNEL_REPO="https://github.com/Lime-Chen/linux-rockchip.git"
 export KERNEL_BRANCH="oracular"
 export KERNEL_FLAVOR="rockchip"
 
-export EXTRA_PPAS="jjriek/rockchip"
+export EXTRA_PPAS="jjriek/rockchip jjriek/rockchip-multimedia"

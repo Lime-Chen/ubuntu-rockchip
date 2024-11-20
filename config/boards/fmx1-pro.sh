f183274ab04fb9b6a3e8035cb21457881a35e358
Lemon1151 <smile1151@foxmail.com>
2024-11-20 19:46:43 +0800

    创建u-boot-rk3399

---
+++ config/boards/fmx1-pro.sh
@@ -0,0 +1,28 @@
+# shellcheck shell=bash
+
+export BOARD_NAME="Fmx1 PRO"
+export BOARD_MAKER="Rockchip"
+export BOARD_SOC="Rockchip RK3399"
+export BOARD_CPU="ARM Cortex A72 / A53"
+export UBOOT_PACKAGE="u-boot-rk3399"
+export UBOOT_RULES_TARGET="fmx1-pro-rk3399"
+export COMPATIBLE_SUITES=("jammy" "noble")
+export COMPATIBLE_FLAVORS=("server" "desktop")
+
+function config_image_hook__fmx1-pro() {
+    local rootfs="$1"
+    local suite="$3"
+
+    if [ "${suite}" == "jammy" ] || [ "${suite}" == "noble" ]; then
+        # Install panfork
+        chroot "${rootfs}" add-apt-repository -y ppa:jjriek/panfork-mesa
+        chroot "${rootfs}" apt-get update
+        chroot "${rootfs}" apt-get -y install mali-t860-firmware
+        chroot "${rootfs}" apt-get -y dist-upgrade
+
+        # Install libmali blobs alongside panfork
+        chroot "${rootfs}" apt-get -y install libmali-t860-x11
+    fi
+
+    return 0
+}
---
+++ packages/u-boot-rk3399/debian/targets.mk
@@ -0,0 +1,7 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+
+u-boot-rockchip_platforms += fmx1-pro-rk3399
+turing-rk1-rk3588_ddr := rk3399_ddr_933MHz_v1.25.bin
+turing-rk1-rk3588_bl31 := rk3399_bl31_v1.35.elf
+turing-rk1-rk3588_pkg := fmx1-pro

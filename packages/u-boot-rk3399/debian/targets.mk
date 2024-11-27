7ac0022689cad300e0c38d55fe8b40d8512437a5
Lime-Chen <lemon1151@163.com>
2024-11-27 19:44:22 +0800

    Update targets.mk

--- packages/u-boot-rk3399/debian/targets.mk
+++ packages/u-boot-rk3399/debian/targets.mk
@@ -2,6 +2,6 @@
 # debian/rules includes this Makefile snippet.
 
 u-boot-rockchip_platforms += fmx1-pro-rk3399
-fmx1-pro-rk3399_ddr := rk3399_ddr_933MHz_v1.25.bin
+fmx1-pro-rk3399_ddr := rk3399_ddr_800MHz_v1.25.bin
 fmx1-pro-rk3399_bl31 := rk3399_bl31_v1.30.elf
 fmx1-pro-rk3399_pkg := fmx1-pro

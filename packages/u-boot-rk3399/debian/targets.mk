ab26dd8a7d166f487fc9f6a8736e285e374c06d3
Lime-Chen <lemon1151@163.com>
2024-11-27 19:22:54 +0800

    Update targets.mk

--- packages/u-boot-rk3399/debian/targets.mk
+++ packages/u-boot-rk3399/debian/targets.mk
@@ -3,5 +3,5 @@
 
 u-boot-rockchip_platforms += fmx1-pro-rk3399
 fmx1-pro-rk3399_ddr := rk3399_ddr_933MHz_v1.25.bin
-fmx1-pro-rk3399_bl31 := rk3399_bl31_v1.35.elf
+fmx1-pro-rk3399_bl31 := rk3399_bl31_v1.30.elf
 fmx1-pro-rk3399_pkg := fmx1-pro

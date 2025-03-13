88964299daeb03bd45de4f7d5c3cc733f853e477
Lime-Chen <lemon1151@163.com>
2025-03-13 20:27:23 +0800

    Update noble.sh

--- config/suites/noble.sh
+++ config/suites/noble.sh
@@ -3,7 +3,7 @@
 export RELASE_NAME="Ubuntu 24.04 LTS (Noble Nombat)"
 export RELASE_VERSION="24.04"
 
-export KERNEL_REPO="https://github.com/Lime-Chen/linux-6.12.y.git"
+export KERNEL_REPO="https://github.com/Lime-Chen/linux-rockchip.git"
 export KERNEL_BRANCH="noble"
 export KERNEL_FLAVOR="rockchip"
 

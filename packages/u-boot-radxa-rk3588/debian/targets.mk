854226c027ef40efac239e39bf9f3d07de9a68f8
Lemon1151 <smile1151@foxmail.com>
2025-05-19 15:50:53 +0800

    Add tb-rk3588sd board support

--- packages/u-boot-radxa-rk3588/debian/targets.mk
+++ packages/u-boot-radxa-rk3588/debian/targets.mk
@@ -144,3 +144,8 @@ u-boot-rockchip_platforms += cyber3588-aib-rk3588
 cyber3588-aib-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
 cyber3588-aib-rk3588_bl31 := rk3588_bl31_v1.45.elf
 cyber3588-aib-rk3588_pkg := cyber3588-aib
+
+u-boot-rockchip_platforms += toybrick-d0-rk3588s
+toybrick-d0-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+toybrick-d0-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+toybrick-d0-rk3588s_pkg := toybrick-d0
